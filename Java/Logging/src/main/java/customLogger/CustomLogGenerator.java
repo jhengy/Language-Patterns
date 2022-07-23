1daf638000031de1bcc15ff053422ee86dab4a68
jhengy <35480886+jhengy@users.noreply.github.com>
2022-07-23 15:57:58 +0800

    Refactor logging

--- Java/Logging/src/main/java/customLogger/CustomLogGenerator.java
+++ Java/Logging/src/main/java/customLogger/CustomLogGenerator.java
@@ -1,10 +1,4 @@
 package customLogger;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class CustomLogGenerator {
-    public static Logger getLoggerForClass(Class<?> clazz) {
-        return LoggerFactory.getLogger(clazz);
-    }
 }
--- Java/Logging/src/test/java/log4j/CustomLoggerTest.java
+++ Java/Logging/src/test/java/log4j/CustomLoggerTest.java
@@ -2,7 +2,7 @@ package log4j;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
-import customLogger.CustomLogRecorder;
+import customLogger.CustomLogger;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,12 +13,12 @@ import java.util.stream.IntStream;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-class CustomLogRecorderTest {
-    Logger logger = LoggerFactory.getLogger(CustomLogRecorderTest.class);
+class CustomLoggerTest {
+    Logger logger = LoggerFactory.getLogger(CustomLoggerTest.class);
 
     @Test
     void testRecordLogForClass() {
-        ListAppender<ILoggingEvent> iLoggingEventListAppender = CustomLogRecorder.recordLogForClass(CustomLogRecorderTest.class);
+        ListAppender<ILoggingEvent> iLoggingEventListAppender = CustomLogger.startRecordingLogs(CustomLoggerTest.class);
 
         int numEvents = 3;
         IntStream.range(0, numEvents)
@@ -37,14 +37,14 @@ class CustomLogRecorderTest {
 
     @Test
     void testGetLoggingEvent() {
-        ListAppender<ILoggingEvent> iLoggingEventListAppender = CustomLogRecorder.recordLogForClass(CustomLogRecorderTest.class);
+        ListAppender<ILoggingEvent> iLoggingEventListAppender = CustomLogger.startRecordingLogs(CustomLoggerTest.class);
         logger.info("log to test with param={}", 1);
         logger.info("log to test with param={}", new TestClass(1));
 
-        List<?> intArgs = CustomLogRecorder.getLoggingEvent(iLoggingEventListAppender, Integer.class);
+        List<?> intArgs = CustomLogger.getArgsListFromLogs(iLoggingEventListAppender, Integer.class);
         assertThat(intArgs.size(), is(1));
 
-        List<?> testClassArgs = CustomLogRecorder.getLoggingEvent(iLoggingEventListAppender, Integer.class);
+        List<?> testClassArgs = CustomLogger.getArgsListFromLogs(iLoggingEventListAppender, Integer.class);
         assertThat(testClassArgs.size(), is(1));
 
     }
