0f688a53ad89dca24e24dbaf897a840003c452b1
jhengy <35480886+jhengy@users.noreply.github.com>
2022-07-23 13:25:40 +0800

    add module for java logging

---
+++ Java/Logging/src/main/java/log4j/CustomLogRecorder.java
@@ -0,0 +1,34 @@
+package log4j;
+
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class CustomLogRecorder {
+    public static ListAppender<ILoggingEvent> recordLogForClass(Class<?> clazz) {
+        Logger logger = (Logger) getLogger(clazz);
+
+        ListAppender<ILoggingEvent> loggingEventListAppender = new ListAppender<>();
+        loggingEventListAppender.start();
+        logger.addAppender(loggingEventListAppender);
+
+        return loggingEventListAppender;
+    }
+
+    // when LogEvents comes with arguments -> filter out certain arguments in the log by type
+    public static List<?> getLoggingEvent(ListAppender<ILoggingEvent> loggingEventListAppender, Class<?> clazz) {
+        return List.copyOf(loggingEventListAppender.list).stream()
+            .filter(event -> event.getArgumentArray() != null)
+            .flatMap(event -> stream(event.getArgumentArray()))
+            .filter(clazz::isInstance)
+            .map(clazz::cast)
+            .collect(Collectors.toList());
+    }
+}
---
+++ Java/Logging/src/test/java/log4j/CustomLogRecorderTest.java
@@ -0,0 +1,60 @@
+package log4j;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class CustomLogRecorderTest {
+    Logger logger = LoggerFactory.getLogger(CustomLogRecorderTest.class);
+
+    @Test
+    void testRecordLogForClass() {
+        ListAppender<ILoggingEvent> iLoggingEventListAppender = CustomLogRecorder.recordLogForClass(CustomLogRecorderTest.class);
+
+        int numEvents = 3;
+        IntStream.range(0, numEvents)
+            .forEach(i -> logger.info("log {}", i));
+
+        List<ILoggingEvent> logs = iLoggingEventListAppender.list;
+        assertThat(logs, hasSize(numEvents));
+        IntStream.range(0, numEvents)
+            .forEach(i -> {
+                ILoggingEvent event = logs.get(i);
+                assertThat(event.getFormattedMessage(), containsString("log"));
+                assertThat(event.getArgumentArray().length, is(1));
+                assertThat(event.getArgumentArray()[0], is(i));
+            });
+    }
+
+    @Test
+    void testGetLoggingEvent() {
+        ListAppender<ILoggingEvent> iLoggingEventListAppender = CustomLogRecorder.recordLogForClass(CustomLogRecorderTest.class);
+        logger.info("log to test with param={}", 1);
+        logger.info("log to test with param={}", new TestClass(1));
+
+        ILoggingEvent iLoggingEvent = iLoggingEventListAppender.list.get(0);
+
+        List<?> intArgs = CustomLogRecorder.getLoggingEvent(iLoggingEventListAppender, Integer.class);
+        assertThat(intArgs.size(), is(1));
+
+        List<?> testClassArgs = CustomLogRecorder.getLoggingEvent(iLoggingEventListAppender, Integer.class);
+        assertThat(testClassArgs.size(), is(1));
+
+    }
+
+    private static class TestClass {
+        int i;
+
+        public TestClass(int i) {
+            this.i = i;
+        }
+    }
+}
