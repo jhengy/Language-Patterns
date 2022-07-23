afc743a14102e77a8e814172752f3615d343308d
jhengy <35480886+jhengy@users.noreply.github.com>
2022-07-23 15:21:00 +0800

    Using mono to turn blocking call to an asynchronous call

--- Java/Concurrency/src/main/java/reactiveCore/SyncToAsync.java
+++ Java/Concurrency/src/main/java/reactiveCore/SyncToAsync.java
@@ -1,4 +1,45 @@
 package reactiveCore;
 
+import org.slf4j.Logger;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.Date;
+
 public class SyncToAsync {
+
+    public static Mono<String> attemptAsync1(int n, long ms, Logger logger) {
+        return Mono.fromSupplier(() -> blockingCall(n, ms, logger));
+    }
+
+    public static Mono<String> attemptAsync2(int n, long ms, Logger logger) {
+        return Mono.fromSupplier(() -> n)
+            .flatMap(i -> Mono.just(blockingCall(n, ms, logger)));
+    }
+
+    public static Mono<String> attemptAsync3(int n, long ms, Logger logger) {
+        return Mono.fromCallable(() -> blockingCall(n, ms, logger)) // Define blocking call
+            .subscribeOn(Schedulers.boundedElastic()); // Define the execution model
+    }
+
+    // Just simulate some delay on the current thread.
+    public static void delay(long timeMs) {
+        try {
+            Thread.sleep(timeMs);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String blockingCall(Integer input, long timeMs, Logger logger) {
+        delay(timeMs);
+
+        String msg = String.format("within blockingCall: [%d] on thread [%s] at time [%s]",
+            input,
+            Thread.currentThread().getName(),
+            new Date());
+
+        logger.info(msg);
+        return msg;
+    }
 }
--- Java/Concurrency/src/test/java/reactiveCore/SyncToAsyncTest.java
+++ Java/Concurrency/src/test/java/reactiveCore/SyncToAsyncTest.java
@@ -1,122 +1,92 @@
 package reactiveCore;
 
-import static org.junit.jupiter.api.Assertions.*;
+import customLogger.CustomLogGenerator;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+
+import java.util.Date;
+
+import static reactiveCore.SyncToAsync.*;
 
 class SyncToAsyncTest {
-    private static final Logger loggerForTestClass = LoggerFactory.getLogger(SyncToAsync.class);
+
+    private static final Logger logger = CustomLogGenerator.getLoggerForClass(SyncToAsyncTest.class);
 
     @Test
     void testAsyncCallUsingReactor() {
         int delayForBlockCalls = 3000;
         int delayAfterBlockCalls = 5000;
 
-        startRecordingLogsFor(FraudAnalyticsWebScoreServiceTest.class);
-
         int ONE = 1;
-        loggerForTestClass.info(String.format("before blockingCall: [%d] on thread [%s] at time [%s]",
+        logger.info(String.format("before blockingCall: [%d] on thread [%s] at time [%s]",
+            ONE,
+            Thread.currentThread().getName(),
+            new Date()));
+
+        attemptAsync1(ONE, delayForBlockCalls, logger)
+            .doOnSuccess((resp) -> logger.info(String.format("upon blockingCall success: [%d] on thread [%s] at time [%s]",
                 ONE,
                 Thread.currentThread().getName(),
-                new Date()));
-
-        attemptAsync1(ONE, delayForBlockCalls)
-                .doOnSuccess((resp) -> loggerForTestClass.info(String.format("upon blockingCall success: [%d] on thread [%s] at time [%s]",
-                        ONE,
-                        Thread.currentThread().getName(),
-                        new Date())))
-                .doOnError(err -> loggerForTestClass.error(String.format("upon blockingCall failure: [%d] on thread [%s] at time [%s]",
-                        ONE,
-                        Thread.currentThread().getName(),
-                        new Date())))
-                .subscribe();
-
-        loggerForTestClass.info(String.format("after blockingCall: [%d] on thread [%s] at time [%s]",
+                new Date())))
+            .doOnError(err -> logger.error(String.format("upon blockingCall failure: [%d] on thread [%s] at time [%s]",
                 ONE,
                 Thread.currentThread().getName(),
-                new Date()));
+                new Date())))
+            .subscribe();
+
+        logger.info(String.format("after blockingCall: [%d] on thread [%s] at time [%s]",
+            ONE,
+            Thread.currentThread().getName(),
+            new Date()));
 
         int TWO = 2;
-        loggerForTestClass.info(String.format("before blockingCall: [%d] on thread [%s] at time [%s]",
+        logger.info(String.format("before blockingCall: [%d] on thread [%s] at time [%s]",
+            TWO,
+            Thread.currentThread().getName(),
+            new Date()));
+
+        attemptAsync2(TWO, delayForBlockCalls, logger)
+            .doOnSuccess((resp) -> logger.info(String.format("upon blockingCall success: [%d] on thread [%s] at time [%s]",
                 TWO,
                 Thread.currentThread().getName(),
-                new Date()));
-
-        attemptAsync2(TWO, delayForBlockCalls)
-                .doOnSuccess((resp) -> loggerForTestClass.info(String.format("upon blockingCall success: [%d] on thread [%s] at time [%s]",
-                        TWO,
-                        Thread.currentThread().getName(),
-                        new Date())))
-                .doOnError(err -> loggerForTestClass.error(String.format("upon blockingCall failure: [%d] on thread [%s] at time [%s]",
-                        TWO,
-                        Thread.currentThread().getName(),
-                        new Date())))
-                .subscribe();
-
-        loggerForTestClass.info(String.format("after blockingCall: [%d] on thread [%s] at time [%s]",
+                new Date())))
+            .doOnError(err -> logger.error(String.format("upon blockingCall failure: [%d] on thread [%s] at time [%s]",
                 TWO,
                 Thread.currentThread().getName(),
-                new Date()));
+                new Date())))
+            .subscribe();
+
+        logger.info(String.format("after blockingCall: [%d] on thread [%s] at time [%s]",
+            TWO,
+            Thread.currentThread().getName(),
+            new Date()));
 
 
         int THREE = 3;
-        loggerForTestClass.info(String.format("before blockingCall: [%d] on thread [%s] at time [%s]",
+        logger.info(String.format("before blockingCall: [%d] on thread [%s] at time [%s]",
+            THREE,
+            Thread.currentThread().getName(),
+            new Date()));
+
+        attemptAsync3(THREE, delayForBlockCalls, logger)
+            .doOnSuccess((resp) -> logger.info(String.format("upon blockingCall success: [%d] on thread [%s] at time [%s]",
                 THREE,
                 Thread.currentThread().getName(),
-                new Date()));
-
-        attemptAsync3(THREE, delayForBlockCalls)
-                .doOnSuccess((resp) -> loggerForTestClass.info(String.format("upon blockingCall success: [%d] on thread [%s] at time [%s]",
-                        THREE,
-                        Thread.currentThread().getName(),
-                        new Date())))
-                .doOnError(err -> loggerForTestClass.error(String.format("upon blockingCall failure: [%d] on thread [%s] at time [%s]",
-                        THREE,
-                        Thread.currentThread().getName(),
-                        new Date())))
-                //            .block();
-                .subscribe();
-
-        loggerForTestClass.info(String.format("after blockingCall: [%d] on thread [%s] at time [%s]",
+                new Date())))
+            .doOnError(err -> logger.error(String.format("upon blockingCall failure: [%d] on thread [%s] at time [%s]",
                 THREE,
                 Thread.currentThread().getName(),
-                new Date()));
+                new Date())))
+            //            .block();
+            .subscribe();
+
+        logger.info(String.format("after blockingCall: [%d] on thread [%s] at time [%s]",
+            THREE,
+            Thread.currentThread().getName(),
+            new Date()));
 
         delay(delayAfterBlockCalls);
 
         // TODO: maybe can assert the order of logs to verify async behaviour
-    }
-
-    private Mono<String> attemptAsync1(int n, long ms) {
-        return Mono.fromSupplier(() -> blockingCall(n, ms));
-    }
-
-    private Mono<String> attemptAsync2(int n, long ms) {
-        return Mono.fromSupplier(() -> n)
-                .flatMap(i -> Mono.just(blockingCall(n, ms)));
-    }
-
-    private Mono<String> attemptAsync3(int n, long ms) {
-        return Mono.fromCallable(() -> blockingCall(n, ms)) // Define blocking call
-                .subscribeOn(Schedulers.boundedElastic()); // Define the execution model
-    }
-
-    // Just simulate some delay on the current thread.
-    public void delay(long timeMs) {
-        try {
-            Thread.sleep(timeMs);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String blockingCall(Integer input, long timeMs) {
-        delay(timeMs);
-
-        String msg = String.format("within blockingCall: [%d] on thread [%s] at time [%s]",
-                input,
-                Thread.currentThread().getName(),
-                new Date());
-
-        loggerForTestClass.info(msg);
-        return msg;
     }
 }
